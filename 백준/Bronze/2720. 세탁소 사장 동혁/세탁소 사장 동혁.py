5a8c38f676185235b9b27190b555c2097dc2bc18
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-03-22 23:14:21 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,9 +1,8 @@
 T = int(input())
-
-for i in range(1,T+1):
-    N = int(input())
-    quater = N // 25
-    dime = (N % 25) // 10
-    nickel = ((N % 25) % 10) // 5
-    penny =((N % 25) % 10) % 5
-    print(quater, dime, nickel, penny)
+for _ in range(T):
+    Money = int(input())
+    amount_list = []
+    for i in [25,10,5,1]:
+        amount_list.append(Money // i)
+        Money %= i
+    print(*amount_list)
