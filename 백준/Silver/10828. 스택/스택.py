3ea8a3bcaf6e632a52bde658e3f3b29dc2bdbe72
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-03-26 17:33:54 +0900

    스택.py
    없던 주석을 달았답니다.

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,3 +1,4 @@
+# 이 상황에서 꼭 sys를 써야하는가? -> Yes. 안 쓰고 Python3으로 돌려봤더니 시간초과 뜸
 import sys
 input = sys.stdin.readline
 
@@ -9,7 +10,12 @@ for _ in range(N):
     if stack[0] == 'push':
         lst.append(stack[1])
     elif stack[0] == 'pop':
+        # 형식 이렇게도 쓸 수 있구나 생각해보기
         print(lst.pop() if lst else -1)
+        # if len(lst) != 0:
+        #    print(lst.pop())
+        # else:
+        #    print(-1)
     elif stack[0] == 'size':
         print(len(lst))
     elif stack[0] == 'empty':
