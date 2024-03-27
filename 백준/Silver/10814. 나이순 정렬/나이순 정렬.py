3d7088709071e916c161f56a4ef07cc297cfe5a5
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-03-27 17:35:34 +0900

    Update 나이순 정렬.py
    주석을 첨부했답니다.

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -4,6 +4,7 @@ input = sys.stdin.readline
 N = int(input())
 
 lst = [list(input().split()) for _ in range(N)]
+# str과 int의 정렬: 20, 200, 21에서 str정렬은 20, 200, 21 순으로 정렬됨. 20, 21, 200 순으로 정렬하려면 int로 변환해줘야 함. 신경쓰기 ! 
 lst.sort(key = lambda x : int(x[0]))
 
 for i in lst:
