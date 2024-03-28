3136c55d5d843980e33e67eed35738b64d015b63
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-03-28 15:54:14 +0900

    카드2.py
    주석을 달았답니다~

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,4 +1,7 @@
 from collections import deque
+# 여기서는 sys.stdin.readline을 쓰나 안 쓰나 시간이 똑같이 걸림. sys.stdin.readline은 입력을 빠르게 받기 위해 사용하는데 입력이 1개밖에 없기 때문에 차이가 없는 거임! 
+# import sys
+# input = sys.stdin.readline
 N = int(input())
 q = deque(range(1,N+1))
 
