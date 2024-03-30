a88d76d262dfb008275693db752ce11523ddb909
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-03-30 17:37:59 +0900

    [Silver IV] Title: 수 찾기, Time: 492 ms, Memory: 47416 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+M = int(input())
+lst = list(map(int, input().split()))
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+for i in lst:
+    result = binary_search(arr, i, 0, N-1)
+    if result == None:
+        print(0)
+    else:
+        print(1)
