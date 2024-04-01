c1469f8e696e6f27057708f86286c549116f76b5
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-04-01 12:42:48 +0900

    Update 체스판 다시 칠하기.py
    좀 더 간결한 풀이 발견
    걍 삘로 else 썼는데 맞음
    그 삘의 근거에 대한 주석을 써놨습니다

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -2,7 +2,7 @@ N, M = map(int, input().split())
 table = [list(input()) for _ in range(N)]
 
 W_lst = ['WBWBWBWB','BWBWBWBW','WBWBWBWB','BWBWBWBW','WBWBWBWB','BWBWBWBW','WBWBWBWB','BWBWBWBW']
-B_lst = ['BWBWBWBW','WBWBWBWB','BWBWBWBW','WBWBWBWB','BWBWBWBW','WBWBWBWB','BWBWBWBW','WBWBWBWB']
+# B_lst = ['BWBWBWBW','WBWBWBWB','BWBWBWBW','WBWBWBWB','BWBWBWBW','WBWBWBWB','BWBWBWBW','WBWBWBWB']
 lst = []
 for row in range(N-7):
     for column in range(M-7):
@@ -12,6 +12,7 @@ for row in range(N-7):
             for j in range(8):
                 if W_lst[i][j] != table[row + i][column + j]:
                     w_count += 1
+                # W_lst하고 B_lst가 반대니까 B_lst 쓸 필요 없이 그냥 else 써도 됨 우와~
                 else:
                     b_count += 1
         lst.append(min(w_count, b_count))
