ff25877f7befa40f8220438ec4fc2194a8b54fb2
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-04-20 20:54:57 +0900

    [Bronze I] Title: ROT13, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,17 @@
+S = input()
+lower = 'abcdefghijklmnopqrstuvwxyz'
+upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def index_finder(case):
+    rot13 = case.index(S[i]) + 13
+    if rot13 >= 26:
+        rot13 %= 13
+    return case[rot13]
+
+for i in range(len(S)):
+    if S[i].isupper() == True:
+        print(index_finder(upper), end ='')
+    elif S[i].islower() == True:
+        print(index_finder(lower), end ='')
+    else:
+        print(S[i], end ='')
