435565eadcab8b80a7699c98c7f0dcd9159ecd58
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-04-21 11:20:14 +0900

    [Silver III] Title: 프린터 큐, Time: 80 ms, Memory: 34264 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,19 @@
+from collections import deque
+import math
+
+n = int(input())
+for _ in range(n):
+    N , M = map(int, input().split())
+    lst = deque(map(int, input().split()))
+    lst[M] += 0.5
+    ans_lst = []
+    while lst:
+        if math.floor(max(lst)) != math.floor(lst[0]):
+            lst.append(lst.popleft())
+        else:
+            ans_lst.append(lst.popleft())
+            if type(ans_lst[-1]) == float:
+                print(len(ans_lst))
+                break
+                
+# 이렇게 푸는 게 아닌 것 같지만 기발하긴 했다
