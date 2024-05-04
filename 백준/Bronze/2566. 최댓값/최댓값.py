84bf1eabc97397e6c269ce9572dece4a9e7fc90c
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-05-04 15:23:41 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,12 +1,11 @@
-table = [list(map(int, input().split())) for _ in range(9)]
-max_num = 0
-max_row, max_col = 0, 0
-for row in range(9):
-    for col in range(9):
-        if max_num <= table[row][col]:
-            max_row = row + 1
-            max_col = col + 1
-            max_num = table[row][col]
-            
-print(max_num)
-print(max_row, max_col)
+num_lst = [] # 90
+index_lst = []
+for i in range(1,10):
+    lst = list(map(int, input().split()))
+    a = max(lst)
+    num_lst.append(a)
+    index_lst.append(lst.index(a))
+x = max(num_lst)
+row = num_lst.index(x)
+print(x)
+print(row+1, index_lst[row]+1)
