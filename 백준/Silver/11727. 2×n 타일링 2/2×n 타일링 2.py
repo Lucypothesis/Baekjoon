f64fde07b3bad52ad71fba5156d0930de191f9b1
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-05-17 15:26:26 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 48 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,16 +1,9 @@
 n = int(input())
 
-def fc(n):
-    a = 1
-    for i in range(1,n+1):
-        a *= i
-    return a
 
-def comb(n,k):
-    return fc(n) // (fc(n-k) * fc(k))
+d = [0] * 1001
+d[1], d[2] = 1, 3
+for n in range(3,n+1):
+    d[n] = d[n-1] + 2 * d[n-2]
 
-answer = 0
-k = n // 2
-for a in range(k+1):
-    answer +=  (2**a) * comb(n-a,a)
-print(int(answer) % 10007)
+print(d[n] % 10007)
